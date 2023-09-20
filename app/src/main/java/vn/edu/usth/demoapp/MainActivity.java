67ab44c638a18bf9629aba09ed518d0b88469e42
Hoang Le <hoanglttbx@gmail.com>
2023-09-21 03:15:31 +0700

    Hoang Le: added About, Setting, Menu, search passing

--- app/src/main/java/vn/edu/usth/demoapp/MainActivity.java
+++ app/src/main/java/vn/edu/usth/demoapp/MainActivity.java
@@ -3,16 +3,24 @@ package vn.edu.usth.demoapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
@@ -26,6 +34,8 @@ import android.widget.Toast;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager2 mViewPager2;
@@ -36,6 +46,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         SharedPreferences sharedPreferences = getSharedPreferences("myKey", MODE_PRIVATE);
         boolean isFirstRun = sharedPreferences.getBoolean("isFirstRun", true);
@@ -86,30 +99,101 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
 
-            // Handle search button @+id/imageButtonSearch
-            findViewById(R.id.imageButtonSearch).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    openSearchDialog(Gravity.CENTER);
-                }
-            });
 
-            findViewById(R.id.imageButtonSettings).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // set isLoggedIn to false
-                    SharedPreferences sharedPreferences = getSharedPreferences("myKey", MODE_PRIVATE);
-                    SharedPreferences.Editor editor = sharedPreferences.edit();
-                    editor.putBoolean("isLoggedIn", false);
-                    editor.apply();
-                    Toast.makeText(MainActivity.this, "Logged out successfully", Toast.LENGTH_SHORT).show();
-                    Intent intent = getIntent();
-                    finish();
-                    startActivity(intent);
-                }
-            });
+            // Handle search button @+id/imageButtonSearch
+//            findViewById(R.id.action_search).setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    openSearchDialog(Gravity.CENTER);
+//                }
+//            });
+//
+//            findViewById(R.id.imageButtonSettings).setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    // set isLoggedIn to false
+//                    SharedPreferences sharedPreferences = getSharedPreferences("myKey", MODE_PRIVATE);
+//                    SharedPreferences.Editor editor = sharedPreferences.edit();
+//                    editor.putBoolean("isLoggedIn", false);
+//                    editor.apply();
+//                    Toast.makeText(MainActivity.this, "Logged out successfully", Toast.LENGTH_SHORT).show();
+//                    Intent intent = getIntent();
+//                    finish();
+//                    startActivity(intent);
+//                }
+//            });
 
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_top, menu);
+
+        // if user is not logged in, hide logout button
+        SharedPreferences sharedPreferences = getSharedPreferences("myKey", MODE_PRIVATE);
+        boolean isLoggedIn = sharedPreferences.getBoolean("isLoggedIn", false);
+        if (!isLoggedIn) {
+            menu.findItem(R.id.action_logout).setVisible(false);
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        if (item.getItemId() == R.id.action_search) {
+            openSearchDialog(Gravity.CENTER);
+            return true;
+        }
+        if (item.getItemId() == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        if (item.getItemId() == R.id.action_about) {
+            Intent intent = new Intent(this, AboutActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        if (item.getItemId() == R.id.action_logout) {
+            SharedPreferences sharedPreferences = getSharedPreferences("myKey", MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean("isLoggedIn", false);
+            editor.apply();
+            Toast.makeText(MainActivity.this, "Logged out successfully", Toast.LENGTH_SHORT).show();
+            Intent intent = getIntent();
+            finish();
+            startActivity(intent);
+            return true;
+        }
+        if (item.getItemId() == R.id.action_exit) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
     }
 
     private void openSearchDialog(int gravity) {
@@ -130,7 +214,7 @@ public class MainActivity extends AppCompatActivity {
 
         dialog.setCancelable(Gravity.BOTTOM == gravity);
 
-        EditText editSeach = dialog.findViewById(R.id.editSearch);
+        EditText editSearch = dialog.findViewById(R.id.editSearch);
         Button buttonCancel = dialog.findViewById(R.id.buttonCancel);
         Button buttonSearch = dialog.findViewById(R.id.buttonSearch);
 
@@ -144,11 +228,19 @@ public class MainActivity extends AppCompatActivity {
         buttonSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Search: " + editSeach.getText().toString(), Toast.LENGTH_SHORT).show();
+                // get search param from editSearch
+                Toast.makeText(MainActivity.this, "Search: " + editSearch.getText().toString(), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, SearchResultActivity.class);
+                Bundle b = new Bundle();
+                b.putString("search_param", editSearch.getText().toString());
+                intent.putExtras(b);
+                startActivity(intent);
+                finish();
             }
         });
 
         dialog.show();
 
     }
+
 }
---
+++ app/src/main/java/vn/edu/usth/demoapp/SettingsActivity.java
@@ -0,0 +1,42 @@
+package vn.edu.usth.demoapp;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.settings, new SettingsFragment())
+                    .commit();
+        }
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        // handle SettingBackButton
+        findViewById(R.id.SettingBackButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        }
+    }
+}
