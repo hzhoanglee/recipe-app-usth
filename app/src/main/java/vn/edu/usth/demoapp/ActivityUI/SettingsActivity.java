ca640b3b2367033f5db88c9ea4a677be7aefacd9
Hoang Le <hoanglttbx@gmail.com>
2023-09-22 13:24:12 +0700

    Hoang Le: Favorite Activity

--- app/src/main/java/vn/edu/usth/demoapp/ActivityUI/SettingsActivity.java
+++ app/src/main/java/vn/edu/usth/demoapp/ActivityUI/SettingsActivity.java
@@ -1,5 +1,6 @@
 package vn.edu.usth.demoapp.ActivityUI;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -51,8 +52,24 @@ public class SettingsActivity extends AppCompatActivity {
         findViewById(R.id.SettingBackButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
                 finish();
             }
         });
+        // TODO: Handle Back Button
+        // on android back action
+        findViewById(R.id.settings).setOnKeyListener((v, keyCode, event) -> {
+            if (keyCode == android.view.KeyEvent.KEYCODE_BACK) {
+                Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+                return true;
+            }
+            return false;
+        });
+
     }
 }
