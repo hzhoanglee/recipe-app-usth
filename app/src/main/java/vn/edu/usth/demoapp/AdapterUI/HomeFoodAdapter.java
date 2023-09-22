1f4a2d31f692be69422fa830df444c1d9d6a3aff
LTH3ar <huylt.bi12-195@st.usth.edu.vn>
2023-09-22 11:04:51 +0700

    layout update Home: Recent

---
+++ app/src/main/java/vn/edu/usth/demoapp/AdapterUI/HomeFoodAdapter.java
@@ -0,0 +1,81 @@
+package vn.edu.usth.demoapp.AdapterUI;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import vn.edu.usth.demoapp.ObjectUI.Food;
+import vn.edu.usth.demoapp.R;
+
+public class HomeFoodAdapter extends RecyclerView.Adapter<HomeFoodAdapter.HomeFoodViewHolder> {
+
+    public Context mContext;
+    public List<Food> mListFood;
+
+    public HomeFoodAdapter(Context mContext) {
+
+        this.mContext = mContext;
+    }
+
+    public void setData(List<Food> list){
+        this.mListFood = list;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public HomeFoodViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_home_item_recent, parent, false);
+
+        return new HomeFoodViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HomeFoodViewHolder holder, int position) {
+        Food food = mListFood.get(position);
+        if(food == null){
+            return;
+        }
+        holder.imgFood.setImageResource(food.getResourceImage());
+        holder.tvName.setText(food.getName());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "You clicked " + food.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if(mListFood !=null){
+            return mListFood.size();
+        }
+        return 0;
+    }
+
+    public class HomeFoodViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView imgFood;
+        private TextView tvName;
+
+        public HomeFoodViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgFood = itemView.findViewById(R.id.img_user_home_recent);
+            tvName = itemView.findViewById(R.id.tv_home_recent_name);
+        }
+    }
+}
--- app/src/main/java/vn/edu/usth/demoapp/FragmentUI/HomeFragment.java
+++ app/src/main/java/vn/edu/usth/demoapp/FragmentUI/HomeFragment.java
@@ -3,18 +3,24 @@ package vn.edu.usth.demoapp.FragmentUI;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 
 import java.util.ArrayList;
 import java.util.List;
 
 import me.relex.circleindicator.CircleIndicator3;
+import vn.edu.usth.demoapp.AdapterUI.FoodAdapter;
+import vn.edu.usth.demoapp.AdapterUI.HomeFoodAdapter;
 import vn.edu.usth.demoapp.AdapterUI.PhotoAdapter;
+import vn.edu.usth.demoapp.ObjectUI.Food;
 import vn.edu.usth.demoapp.ObjectUI.Photo;
 import vn.edu.usth.demoapp.R;
 
@@ -23,6 +29,12 @@ public class HomeFragment extends Fragment {
     private View mView;
     private ViewPager2 CarouselViewPager;
     private CircleIndicator3 CarouselIndicator;
+    private HomeFoodAdapter foodAdapter;
+
+    private RecyclerView recyclerViewCategory;
+    private RecyclerView recyclerViewRecent;
+    private RecyclerView recyclerViewVideo;
+
 
 
     List<String> imageList;
@@ -33,10 +45,18 @@ public class HomeFragment extends Fragment {
         mView = inflater.inflate(R.layout.fragment_home, container, false);
         CarouselViewPager = mView.findViewById(R.id.viewPagerImageSlider);
         CarouselIndicator = mView.findViewById(R.id.carousel_indicator);
+        recyclerViewCategory = mView.findViewById(R.id.recycler_view_category);
+        recyclerViewRecent = mView.findViewById(R.id.recycler_view_recent);
+        recyclerViewVideo = mView.findViewById(R.id.recycler_view_video);
 
         PhotoAdapter photoAdapter = new PhotoAdapter(requireActivity(), getListPhoto());
         CarouselViewPager.setAdapter(photoAdapter);
         CarouselIndicator.setViewPager(CarouselViewPager);
+        foodAdapter = new HomeFoodAdapter(requireContext());
+        // set recent food(horizontal scroll)
+        recyclerViewRecent.setLayoutManager(new LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false));
+        foodAdapter.setData(getListRecentFood());
+        recyclerViewRecent.setAdapter(foodAdapter);
 
         return mView;
     }
@@ -46,6 +66,39 @@ public class HomeFragment extends Fragment {
         list.add(new Photo(R.drawable.place_holder, "Steak1"));
         list.add(new Photo(R.drawable.place_holder, "Steak2"));
         list.add(new Photo(R.drawable.place_holder, "Steak3"));
+        return list;
+    }
+
+    private List<Food> getListTypeFood(){
+        List<Food> list = new ArrayList<>();
+        list.add(new Food(R.drawable.appetizers, "Appetizers", 0, "This is appetizers recipe that you can make at home"));
+        list.add(new Food(R.drawable.breakfast, "Breakfast", 0, "This is breakfast recipe that you can make at home"));
+        list.add(new Food(R.drawable.main_dish, "Main dish", 0, "This is main dish recipe that you can make at home"));
+        list.add(new Food(R.drawable.side_dish, "Side dish", 0, "This is side dish recipe that you can make at home"));
+        list.add(new Food(R.drawable.desserts, "Desserts", 0, "This is desserts recipe that you can make at home"));
+        list.add(new Food(R.drawable.drinks, "Drinks", 0, "This is drinks recipe that you can make at home"));
+        return list;
+    }
+
+    private List<Food> getListRecentFood(){
+        List<Food> list = new ArrayList<>();
+        list.add(new Food(R.drawable.appetizers, "Appetizers", 0, "This is appetizers recipe that you can make at home"));
+        list.add(new Food(R.drawable.breakfast, "Breakfast", 0, "This is breakfast recipe that you can make at home"));
+        list.add(new Food(R.drawable.main_dish, "Main dish", 0, "This is main dish recipe that you can make at home"));
+        list.add(new Food(R.drawable.side_dish, "Side dish", 0, "This is side dish recipe that you can make at home"));
+        list.add(new Food(R.drawable.desserts, "Desserts", 0, "This is desserts recipe that you can make at home"));
+        list.add(new Food(R.drawable.drinks, "Drinks", 0, "This is drinks recipe that you can make at home"));
+        return list;
+    }
+
+    private List<Food> getListVideoFood(){
+        List<Food> list = new ArrayList<>();
+        list.add(new Food(R.drawable.appetizers, "Appetizers", 0, "This is appetizers recipe that you can make at home"));
+        list.add(new Food(R.drawable.breakfast, "Breakfast", 0, "This is breakfast recipe that you can make at home"));
+        list.add(new Food(R.drawable.main_dish, "Main dish", 0, "This is main dish recipe that you can make at home"));
+        list.add(new Food(R.drawable.side_dish, "Side dish", 0, "This is side dish recipe that you can make at home"));
+        list.add(new Food(R.drawable.desserts, "Desserts", 0, "This is desserts recipe that you can make at home"));
+        list.add(new Food(R.drawable.drinks, "Drinks", 0, "This is drinks recipe that you can make at home"));
         return list;
     }
 }
