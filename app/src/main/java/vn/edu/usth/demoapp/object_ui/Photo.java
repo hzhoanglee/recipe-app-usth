3830d3948e2c900243dfa930a2f7fbe751b094ec
LTH3ar <huylt.bi12-195@st.usth.edu.vn>
2023-10-06 21:41:17 +0700

    HuyDev: quick fix Carousel

--- app/src/main/java/vn/edu/usth/demoapp/object_ui/Photo.java
+++ app/src/main/java/vn/edu/usth/demoapp/object_ui/Photo.java
@@ -1,6 +1,9 @@
 package vn.edu.usth.demoapp.object_ui;
 
 import android.text.Html;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -8,6 +11,7 @@ import org.json.JSONObject;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Photo implements Serializable {
@@ -25,9 +29,10 @@ public class Photo implements Serializable {
     private String serving;
     private String level;
     private String html_content;
+    private boolean isFavourite;
 
 
-    public Photo(Integer id, String urlImage, String name, float star, String description, String category, String[] ingredients, String[] steps, String prepTime, String cookTime, String serving, String level, String html_content) {
+    public Photo(Integer id, String urlImage, String name, float star, String description, String category, String[] ingredients, String[] steps, String prepTime, String cookTime, String serving, String level, String html_content, boolean isFavourite) {
         this.id = id;
         this.urlImage = urlImage;
         this.name = name;
@@ -41,6 +46,7 @@ public class Photo implements Serializable {
         this.serving = serving;
         this.level = level;
         this.html_content = html_content;
+        this.isFavourite = isFavourite;
     }
 
     public static List<Photo> fromJson(String json) throws JSONException {
@@ -74,8 +80,50 @@ public class Photo implements Serializable {
             String serving = foodObj.getString("serving");
             String level = foodObj.getString("level");
             String html_content = foodObj.getString("html");
+            boolean isFavourite = foodObj.getBoolean("is_my_favourite");
 
-            Photo food = new Photo(id, image, name, star, description, category, ingredients, steps, prepTime, cookTime, serving, level, html_content);
+            Photo food = new Photo(id, image, name, star, description, category, ingredients, steps, prepTime, cookTime, serving, level, html_content, isFavourite);
+            foods.add(food);
+        }
+
+        return foods;
+    }
+
+    public static List<Photo> fromFavouriteJson(String json) throws JSONException {
+        List<Photo> foods = new ArrayList<>();
+
+        JSONObject obj = new JSONObject(json);
+        JSONArray data = obj.getJSONArray("data");
+
+        for(int i = 0; i < data.length(); i++) {
+            JSONObject favObject = data.getJSONObject(i);
+            JSONObject foodObj = favObject.getJSONObject("recipe");
+
+            int id = foodObj.getInt("id");
+            String name = foodObj.getString("name");
+            String description = foodObj.getString("description");
+            String image = foodObj.getString("image");
+            float star = (float) foodObj.getDouble("review");
+            JSONObject categoryObj = foodObj.getJSONObject("category");
+            String category = categoryObj.getString("name");
+            JSONArray ingredientsArr = foodObj.getJSONArray("ingredients");
+            String[] ingredients = new String[ingredientsArr.length()];
+            for(int j = 0; j < ingredientsArr.length(); j++) {
+                ingredients[j] = ingredientsArr.getString(j);
+            }
+            JSONArray stepsArr = foodObj.getJSONArray("steps");
+            String[] steps = new String[stepsArr.length()];
+            for(int j = 0; j < stepsArr.length(); j++) {
+                steps[j] = stepsArr.getString(j);
+            }
+            String prepTime = foodObj.getString("prep_time");
+            String cookTime = foodObj.getString("cook_time");
+            String serving = foodObj.getString("serving");
+            String level = foodObj.getString("level");
+            String html_content = foodObj.getString("html");
+            boolean isFavourite = foodObj.getBoolean("is_my_favourite");
+
+            Photo food = new Photo(id, image, name, star, description, category, ingredients, steps, prepTime, cookTime, serving, level, html_content, isFavourite);
             foods.add(food);
         }
 
@@ -148,5 +196,27 @@ public class Photo implements Serializable {
 
     public Integer getId() {
         return id;
+    }
+
+    public boolean isFavourite() {
+        return isFavourite;
+    }
+
+    @NonNull
+    public String toString() {
+        return "Name: " + name + "\n" +
+                "Description: " + description + "\n" +
+                "Image: " + urlImage + "\n" +
+                "Star: " + star + "\n" +
+                "Category: " + category + "\n" +
+                "Ingredients: " + Arrays.toString(ingredients) + "\n" +
+                "Steps: " + Arrays.toString(steps) + "\n" +
+                "Prep time: " + prepTime + "\n" +
+                "Cook time: " + cookTime + "\n" +
+                "Serving: " + serving + "\n" +
+                "Level: " + level + "\n" +
+//                "Html content: " + html_content + "\n" +
+                "Is favourite: " + isFavourite + "\n";
+
     }
 }
